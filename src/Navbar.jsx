94ce189aebbbb8863a9a5f2792645a01d1fc4780
fissid <fissid.ss@gmail.com>
2023-12-14 12:04:14 +0300

    updates

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,17 +1,28 @@
-import { useState } from "react";
+import Search from "./Search";
 
 export default function Navbar() {
-  const [query, setQuery] = useState("");
   return (
     <nav className="nav-bar">
-      <div className="logo">
-        <span role="img">🍿</span>
-        <h1>usePopcorn</h1>
-      </div>
-      <input className="search" type="text" placeholder="Search movies..." value={query} onChange={(e) => setQuery(e.target.value)} />
-      <p className="num-results">
-        Found <strong>X</strong> results
-      </p>
+      <Logo />
+      <Search />
+      <NumResults />
     </nav>
+  );
+}
+
+function Logo() {
+  return (
+    <div className="logo">
+      <span role="img">🍿</span>
+      <h1>usePopcorn</h1>
+    </div>
+  );
+}
+
+function NumResults() {
+  return (
+    <p className="num-results">
+      Found <strong>X</strong> results
+    </p>
   );
 }
