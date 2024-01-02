c512005b0730ade948ccb785b0ba67dec1ae5700
fissid <fissid.ss@gmail.com>
2024-01-02 18:11:30 +0300

    updates

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,3 +1,6 @@
+import { useRef } from "react";
+
 export default function Search({ query, setQuery }) {
+  const inputEl = useRef(null);
   return <input className="search" type="text" placeholder="Search movies..." value={query} onChange={(e) => setQuery(e.target.value)} />;
 }
