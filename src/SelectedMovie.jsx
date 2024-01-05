20943ece97df714055978573885c9f2f22252708
fissid <fissid.ss@gmail.com>
2024-01-06 00:04:47 +0300

    updates

--- src/SelectedMovie.jsx
+++ src/SelectedMovie.jsx
@@ -48,7 +48,7 @@ export default function SelecetdMovie({ selectedId, onCloseMovie, getQuery, onAd
     [title, year]
   );
 
-  useKey("Escape", onCloseMovie);
+  useKey("escape", onCloseMovie);
 
   return (
     <div className="details">
