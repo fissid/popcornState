3a1cf3ce2dbdddd6095787b305ec47935dd62cc7
fissid <fissid.ss@gmail.com>
2024-01-06 14:20:28 +0300

    geolocation

--- src/Geolocation.jsx
+++ src/Geolocation.jsx
@@ -1,8 +1,36 @@
-import { useGeolocation } from "./useGeolocation";
+import { useState } from "react";
+
+function useGeolocation() {}
 
 export default function App() {
-  const { position, isLoading, countClicks, error, getPosition } = useGeolocation();
+  const [isLoading, setIsLoading] = useState(false);
+  const [countClicks, setCountClicks] = useState(0);
+  const [position, setPosition] = useState({});
+  const [error, setError] = useState(null);
+
   const { lat, lng } = position;
+
+  function getPosition() {
+    setCountClicks((count) => count + 1);
+
+    if (!navigator.geolocation) return setError("Your browser does not support geolocation");
+
+    setIsLoading(true);
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        setPosition({
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude,
+        });
+        setIsLoading(false);
+      },
+      (error) => {
+        setError(error.message);
+        setIsLoading(false);
+      }
+    );
+  }
+
   return (
     <div>
       <button onClick={getPosition} disabled={isLoading}>
