f506f414ed9456921ef3db3229797a0066fc760a
fissid <fissid.ss@gmail.com>
2024-01-06 14:53:13 +0300

    geolocation

--- src/useGeolocation.jsx
+++ src/useGeolocation.jsx
@@ -4,4 +4,26 @@ export function useGeolocation() {
   const [isLoading, setIsLoading] = useState(false);
   const [position, setPosition] = useState({});
   const [error, setError] = useState(null);
+
+  function getPosition() {
+    // setCountClicks((count) => count + 1);
+
+    if (!navigator.geolocation) return setError("Your browser does not support geolocation");
+
+    setIsLoading(true);
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        setPosition({
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude,
+        });
+        setIsLoading(false);
+      },
+      (error) => {
+        setError(error.message);
+        setIsLoading(false);
+      }
+    );
+  }
+  return { getPosition, isLoading, position, error };
 }
